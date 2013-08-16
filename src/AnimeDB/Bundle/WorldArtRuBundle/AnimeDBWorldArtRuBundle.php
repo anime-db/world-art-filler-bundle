dac62a6047a33cb4761c3dacc42e9f05219690ab
Peter Gribanov <info@peter-gribanov.ru>
2013-08-16 10:34:57 +0400

    add package source

---
+++ src/AnimeDB/Bundle/WorldArtRuBundle/AnimeDBWorldArtRuBundle.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * AnimeDB package
+ *
+ * @package   AnimeDB
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDB\Bundle\WorldArtRuBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle
+ *
+ * @package AnimeDB\Bundle\WorldArtRuBundle
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class AnimeDBWorldArtRuBundle extends Bundle
+{
+}
