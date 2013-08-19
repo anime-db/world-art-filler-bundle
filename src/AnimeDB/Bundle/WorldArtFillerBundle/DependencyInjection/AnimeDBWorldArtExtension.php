769799786de06865605a5e7ef10dbc5f8b461c61
Peter Gribanov <info@peter-gribanov.ru>
2013-08-19 11:02:15 +0400

    rename bundle WorldArtBundle into WorldArtFillerBundle

--- src/AnimeDB/Bundle/WorldArtFillerBundle/DependencyInjection/AnimeDBWorldArtExtension.php
+++ src/AnimeDB/Bundle/WorldArtFillerBundle/DependencyInjection/AnimeDBWorldArtExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AnimeDB\Bundle\WorldArtBundle\DependencyInjection;
+namespace AnimeDB\Bundle\WorldArtFillerBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
