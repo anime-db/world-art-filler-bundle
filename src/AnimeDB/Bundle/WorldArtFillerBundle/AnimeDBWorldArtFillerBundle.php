141c2ded7cdae63e72c1f0a87307d5bd308e582e
Peter Gribanov <info@peter-gribanov.ru>
2013-10-03 10:32:22 +0400

    rename vendor

--- src/AnimeDB/Bundle/WorldArtFillerBundle/AnimeDBWorldArtFillerBundle.php
+++ src/AnimeDB/Bundle/WorldArtFillerBundle/AnimeDBWorldArtFillerBundle.php
@@ -1,23 +1,23 @@
 <?php
 /**
- * AnimeDB package
+ * AnimeDb package
  *
- * @package   AnimeDB
+ * @package   AnimeDb
  * @author    Peter Gribanov <info@peter-gribanov.ru>
  * @copyright Copyright (c) 2011, Peter Gribanov
  * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
  */
 
-namespace AnimeDB\Bundle\WorldArtFillerBundle;
+namespace AnimeDb\Bundle\WorldArtFillerBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * Bundle
  *
- * @package AnimeDB\Bundle\WorldArtFillerBundle
+ * @package AnimeDb\Bundle\WorldArtFillerBundle
  * @author  Peter Gribanov <info@peter-gribanov.ru>
  */
-class AnimeDBWorldArtFillerBundle extends Bundle
+class AnimeDbWorldArtFillerBundle extends Bundle
 {
 }
